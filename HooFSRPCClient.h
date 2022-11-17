a797fa71192c513eb2879460ea7f3bcf04a68541
austinlehman <austinlehman@gmail.com>
2022-11-16 19:54:48 -0600

    Started work on client

--- HooFSRPCClient.h
+++ HooFSRPCClient.h
@@ -9,5 +9,30 @@
 #define HooFSRPCClient_h
 
 #include <stdio.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/dir.h>
+#include <sys/stat.h>
+
+using namespace std;
+
+class HooFSRPCClient {
+    int cPort;
+    int serverPort;
+    string serverIP;
+    
+    HooFSRPCClient(int cPort, int serverPort, string serverIP);
+    ~HooFSRPCClient();
+    
+    dirent *readdir(char *path);
+    int open(char *path, int flags);
+    int create(char *path, int mode);
+    int unlink(char *path);
+    int release(int fd);
+    struct stat *getAttr(char *path, struct stat *stbuf);
+    int rmdir(char *path);
+    int read(int fd, int size, int offset);
+    int write(int fd, char *buf, int size, int offset);
+};
 
 #endif /* HooFSRPCClient_h */
