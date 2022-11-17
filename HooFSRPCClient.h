3273db03eb82e5733bc6089f2b992739c8c47c7b
austinlehman <austinlehman@gmail.com>
2022-11-16 21:14:34 -0600

    added string constants

--- HooFSRPCClient.h
+++ HooFSRPCClient.h
@@ -27,6 +27,22 @@ class HooFSRPCClient {
     int serverPort;
     string serverIP;
     
+    const string _readdir = "rpc.readdir";
+    const string _open = "rpc.open";
+    const string _create = "rpc.create";
+    const string _unlink = "rpc.unlink";
+    const string _release = "rpc.release";
+    const string _getAttr = "rpc.getattr";
+    const string _rmdir = "rpc.rmdir";
+    const string _read = "rpc.read";
+    const string _write = "rpc.write";
+    const string _setxattr = "rpc.setxattr";
+    const string _chmod = "rpc.chmod";
+    const string _chown = "rpc.chown";
+    const string _utime = "rpc.utime";
+    const string _truncate = "rpc.truncate";
+    
+    
     HooFSRPCClient(string ip, int serverPort, int clientPort);
     ~HooFSRPCClient();
     
